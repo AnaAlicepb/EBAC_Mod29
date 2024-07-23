fe8458e5f6e30aac5d31d963c8620d7425eef58b
Ana Alice <anaeanali5@gmail.com>
2024-07-23 09:44:05 -0300

    ajustando projeto

--- ExemplosJDBCAlunos/src/java/main/br/com/ana/dao/ClienteDAO.java
+++ ExemplosJDBCAlunos/src/java/main/br/com/ana/dao/ClienteDAO.java
@@ -3,6 +3,8 @@ package br.com.ana.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.ana.dao.jdbc.ConnectionFactory;
 import br.com.ana.domain.Cliente;
@@ -54,6 +56,9 @@ public class ClienteDAO implements IClienteDAO {
         } catch(Exception e) {
             throw e;
         } finally {
+            if (rs != null && !rs.isClosed()) {
+                rs.close();
+            }
             if (stm != null && !stm.isClosed()) {
                 stm.close();
             }
@@ -72,6 +77,63 @@ public class ClienteDAO implements IClienteDAO {
             String sql = "DELETE FROM cliente WHERE codigo = ?";
             stm = connection.prepareStatement(sql);
             stm.setString(1, cliente.getCodigo());
+            return stm.executeUpdate();
+        } catch(Exception e) {
+            throw e;
+        } finally {
+            if (stm != null && !stm.isClosed()) {
+                stm.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Cliente> consultarTodos() throws Exception {
+        Connection connection = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        List<Cliente> clientes = new ArrayList<>();
+        try {
+            connection = ConnectionFactory.getConnection();
+            String sql = "SELECT * FROM cliente";
+            stm = connection.prepareStatement(sql);
+            rs = stm.executeQuery();
+            while (rs.next()) {
+                Cliente cliente = new Cliente();
+                cliente.setId(rs.getLong("id"));
+                cliente.setCodigo(rs.getString("codigo"));
+                cliente.setNome(rs.getString("nome"));
+                clientes.add(cliente);
+            }
+            return clientes;
+        } catch(Exception e) {
+            throw e;
+        } finally {
+            if (rs != null && !rs.isClosed()) {
+                rs.close();
+            }
+            if (stm != null && !stm.isClosed()) {
+                stm.close();
+            }
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    public Integer atualizar(Cliente cliente) throws Exception {
+        Connection connection = null;
+        PreparedStatement stm = null;
+        try {
+            connection = ConnectionFactory.getConnection();
+            String sql = "UPDATE cliente SET nome = ? WHERE codigo = ?";
+            stm = connection.prepareStatement(sql);
+            stm.setString(1, cliente.getNome());
+            stm.setString(2, cliente.getCodigo());
             return stm.executeUpdate();
         } catch(Exception e) {
             throw e;
